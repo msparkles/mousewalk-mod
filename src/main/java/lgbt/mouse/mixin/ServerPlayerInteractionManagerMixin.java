77d524f63c346dbd5ce54b381d45ebb94cb5c34a
Madeline Sparkles <madeline@mouse.lgbt>
2024-01-13 16:29:44 +0800

    DRILL NO LONGER JUST SIMPLY REMOVES BEDROCK THIS IS FUCKING MOJANK

--- src/main/java/lgbt/mouse/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/lgbt/mouse/mixin/ServerPlayerInteractionManagerMixin.java
@@ -1,9 +1,11 @@
 package lgbt.mouse.mixin;
 
 import lgbt.mouse.items.IndustrialDrill;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
@@ -24,13 +26,20 @@ public abstract class ServerPlayerInteractionManagerMixin {
     @Shadow
     public abstract boolean tryBreakBlock(BlockPos pos);
 
+    @Shadow
+    protected ServerWorld world;
+
     @Inject(method = "finishMining", at = @At("HEAD"))
     public void $mousewalk_finishMining(BlockPos pos, int sequence, String reason, CallbackInfo ci) {
-        if (this.player.getMainHandStack().getItem() == IndustrialDrill.INSTANCE) {
+        if (player.getMainHandStack().getItem() == IndustrialDrill.INSTANCE) {
             HitResult cast = player.raycast(PlayerEntity.getReachDistance(player.isCreative()), 0F, false);
+            
             if (cast instanceof BlockHitResult result) {
-                for (BlockPos n : IndustrialDrill.INSTANCE.findBlocks(player, pos, player.getServerWorld(), result.getSide())) {
-                    this.tryBreakBlock(n);
+                for (BlockPos n : IndustrialDrill.INSTANCE.findBlocks(player, pos, world, result.getSide())) {
+                    BlockState state = world.getBlockState(n);
+                    if (state != null && state.calcBlockBreakingDelta(player, world, n) > 0.0) {
+                        this.tryBreakBlock(n);
+                    }
                 }
             }
         }
@@ -38,8 +47,8 @@ public abstract class ServerPlayerInteractionManagerMixin {
 
     @Inject(method = "tryBreakBlock", at = @At("HEAD"), cancellable = true)
     public void $mousewalk_tryBreakBlock(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        if (this.player.getMainHandStack().getItem() == IndustrialDrill.INSTANCE
-                && IndustrialDrill.INSTANCE.calculateRemainingFuel(this.player.getMainHandStack()) <= 0) {
+        if (player.getMainHandStack().getItem() == IndustrialDrill.INSTANCE
+                && IndustrialDrill.INSTANCE.calculateRemainingFuel(player.getMainHandStack()) <= 0) {
             cir.setReturnValue(false);
         }
     }
