a01667f0b337a27a49eefc2acc2f107cb94d2816
Madeline Sparkles <madeline@mouse.lgbt>
2024-01-16 16:59:38 +0800

    polymer item group

--- src/main/kotlin/lgbt/mouse/Mousewalk.kt
+++ src/main/kotlin/lgbt/mouse/Mousewalk.kt
@@ -1,5 +1,6 @@
 package lgbt.mouse
 
+import eu.pb4.polymer.core.api.item.PolymerItemGroupUtils
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils
 import lgbt.mouse.blocks.COPPER_CABLE_ID
 import lgbt.mouse.blocks.CopperCable
@@ -10,8 +11,11 @@ import lgbt.mouse.items.Estrogen
 import lgbt.mouse.items.IndustrialDrill
 import lgbt.mouse.utils.BlockHighlighter
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
+import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
@@ -21,14 +25,13 @@ object Mousewalk : ModInitializer {
     val LOGGER = LoggerFactory.getLogger(MOD_ID)!!
 
     override fun onInitialize() {
-        PolymerResourcePackUtils.addModAssets(MOD_ID)
         PolymerResourcePackUtils.markAsRequired()
 
         BlockHighlighter.register()
 
+        Registry.register(Registries.ITEM, Identifier(MOD_ID, "industrial_drill"), IndustrialDrill)
         Registry.register(Registries.ITEM, Identifier(MOD_ID, "builders_wand"), BuildersWand)
         Registry.register(Registries.ITEM, Identifier(MOD_ID, "estrogen"), Estrogen)
-        Registry.register(Registries.ITEM, Identifier(MOD_ID, "industrial_drill"), IndustrialDrill)
         Registry.register(Registries.ITEM, Identifier(MOD_ID, "dolly"), Dolly)
 
         Registry.register(Registries.BLOCK, COPPER_CABLE_ID, CopperCable)
@@ -38,6 +41,22 @@ object Mousewalk : ModInitializer {
             Registries.STATUS_EFFECT,
             Identifier(MOD_ID, "double_jump"),
             DoubleJumpStatusEffect
+        )
+
+        PolymerItemGroupUtils.registerPolymerItemGroup(
+            Identifier(MOD_ID, "main"),
+            FabricItemGroup
+                .builder()
+                .icon { ItemStack(IndustrialDrill) }
+                .displayName(Text.translatable("itemGroup.mousewalk.main"))
+                .entries { _, entries ->
+                    entries.add(BuildersWand)
+                    entries.add(Estrogen)
+                    entries.add(IndustrialDrill)
+                    entries.add(Dolly)
+                    entries.add(CopperCable.Item)
+                }
+                .build()
         )
 
         DoubleJumpStatusEffect.register()
